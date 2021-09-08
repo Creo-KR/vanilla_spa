335d9693276c8fac9f839e9febcb49c815a58851
Creo-KR <gjwlsdn0877@naver.com>
2021-09-08 15:20:56 +0900

    tab

--- public/src/components/tab.js
+++ public/src/components/tab.js
@@ -1,7 +1,15 @@
 import Component from "../core/component.js";
 
 export default class Tab extends Component {
-  state = { index: 0, tabs: [{ name: "Tab1", contents: "" }], ...this.state };
+  state = {
+    index: 0,
+    tabs: [{ name: "Tab1", contents: "" }],
+    buttons: { add: false, remove: false },
+    ...this.state,
+  };
+
+  event = { add: false, remove: false, ...this.event };
+
   async init() {
     this.className = "tab";
   }
@@ -10,7 +18,14 @@ export default class Tab extends Component {
     this.$target.addEventListener("click", (e) => {
       let $tabButton = e.target.closest(".tab-button");
       if ($tabButton) {
-        console.log($tabButton.dataset["tabIndex"]);
+        if ($tabButton.classList.contains("add")) {
+          this.event.add && this.event.add();
+        } else {
+          let index = $tabButton.dataset["index"] * 1;
+          if (e.target.closest(".remove"))
+            this.event.remove && this.event.remove(index);
+          else this.setState({ index });
+        }
       }
     });
   }
@@ -22,13 +37,33 @@ export default class Tab extends Component {
     this.$tabList.className = "tab-list";
     this.$target.appendChild(this.$tabList);
 
+    if (this.state.index < 0) this.state.index = 0;
+    let removeVisible = this.state.buttons.remove && this.state.tabs.length > 1;
+
     this.state.tabs.map((tab, index) => {
-      let $tab = document.createElement("li");
-      $tab.classList.add("tab-button");
-      $tab.classList.add("on");
-      $tab.dataset["tabIndex"] = index;
-      $tab.innerText = tab.name;
-      this.$tabList.appendChild($tab);
+      let $tabButton = document.createElement("li");
+      $tabButton.classList.add("tab-button");
+      if (this.state.index == index) $tabButton.classList.add("on");
+      $tabButton.dataset["index"] = index;
+
+      let html = tab.name;
+      if (removeVisible) html += `<span class="remove">✖</span>`;
+      $tabButton.innerHTML = html;
+      this.$tabList.appendChild($tabButton);
     });
+
+    if (this.state.buttons.add) {
+      let $tabButton = document.createElement("li");
+      $tabButton.classList.add("tab-button");
+      $tabButton.classList.add("add");
+      $tabButton.dataset["id"] = "add";
+      $tabButton.innerText = "➕";
+      this.$tabList.appendChild($tabButton);
+    }
+
+    this.$tabContents = document.createElement("div");
+    this.$tabContents.className = "tab-contents";
+    this.$target.appendChild(this.$tabContents);
+    this.$tabContents.innerHTML = this.state.tabs[this.state.index].contents;
   }
 }
--- public/src/pages/note.js
+++ public/src/pages/note.js
@@ -2,11 +2,15 @@ import Tab from "../components/tab.js";
 import Component from "../core/component.js";
 
 export default class NotePage extends Component {
+  state = { tabs: [{ name: "TAB0", contents: "first tab" }], ...this.state };
+
+  seq = 1;
+
   async init() {
     this.className = "page note";
 
     const data = localStorage["data-note"];
-    if (data) this.state = { data: JSON.parse(data) };
+    if (data) this.state = JSON.parse(data);
   }
 
   template() {
@@ -20,6 +24,35 @@ export default class NotePage extends Component {
     this.$title.innerText = "Note Page";
     this.$target.appendChild(this.$title);
 
-    this.tab = new Tab({ parent: this, initialState: {} });
+    this.tab = new Tab({
+      parent: this,
+      initialState: {
+        tabs: this.state.tabs,
+        buttons: { add: true, remove: true },
+      },
+      event: { add: this.addTab, remove: this.removeTab },
+    });
   }
+
+  addTab = () => {
+    this.state.tabs.push({
+      name: `TAB${this.seq++}`,
+      contents: `created on ${new Date().toISOString()}`,
+    });
+    this.tab.setState({ index: this.state.tabs.length - 1 });
+
+    localStorage["data-note"] = JSON.stringify(this.state);
+  };
+
+  removeTab = (index) => {
+    this.state.tabs.splice(index, 1);
+
+    let newState = {};
+    if (this.tab.state.index >= index)
+      newState.index = this.tab.state.index - 1;
+
+    this.tab.setState(newState);
+
+    localStorage["data-note"] = JSON.stringify(this.state);
+  };
 }
