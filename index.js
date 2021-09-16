98ea023a97ef23a1bff414f032b841ba54b6ffc7
Creo-KR <gjwlsdn0877@naver.com>
2021-09-16 14:27:45 +0900

    seo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-const { getMetaData } = require("./seo.js");
+const seo = require("./seo.js");
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
@@ -15,10 +15,9 @@ app.get("/", (req, res) => {
 app.get("/spa/*", (req, res) => {
   res.send(`<html>
 <head>
-    <title>SPA 페이지</title>
     <link rel="stylesheet" href="../src/styles/reset.css">
     <link rel="stylesheet" href="../src/styles/style.css">
-    ${getMetaData()}
+    ${seo.getMetaData(req.originalUrl)}
     <script type="module" src="../src/index.js"></script>
 </head>
 <body>
--- seo.js
+++ seo.js
@@ -1,20 +1,46 @@
-const routes = {};
+const routes = {
+  "": {
+    title: "SPA MAIN",
+    description: "Main Page",
+    url: "/spa/",
+    imageUrl: "/images/01.jpg",
+  },
 
-export const getMetaData = (path) => {
-  const { title, description, url, imageUrl } = routes[path];
-  return `
-          <title>${title}</title>
-          <meta name="title" content="${title}" />
-          <meta name="description" content="${description}" />
-          <meta property="og:url" content="${url}" />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="${title}" />
-          <meta property="og:description" content="${description}" />
-          <meta property="og:image" content="${imageUrl}" />
-          <meta property="twitter:card" content="${imageUrl}" />
-          <meta property="twitter:url" content="${url}" />
-          <meta property="twitter:title" content="${title}" />
-          <meta property="twitter:description" content="${description}" />
-          <meta property="twitter:image" content="${imageUrl}" />
-        `;
+  gallery: {
+    title: "SPA GALLERY",
+    description: "Gallery Page",
+    url: "/spa/gallery",
+    imageUrl: "/images/02.jpg",
+  },
+
+  note: {
+    title: "SPA NOTE",
+    description: "Note Page",
+    url: "/spa/note",
+    imageUrl: "/images/03.jpg",
+  },
 };
+
+const getMetaData = (path) => {
+  path = path.replace(/\/spa\//, "");
+  const route = routes[path];
+  if (route)
+    return `
+          <title>${route.title}</title>
+          <meta name="title" content="${route.title}" />
+          <meta name="description" content="${route.description}" />
+          <meta property="og:url" content="${route.url}" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="${route.title}" />
+          <meta property="og:description" content="${route.description}" />
+          <meta property="og:image" content="${route.imageUrl}" />
+          <meta property="twitter:card" content="${route.imageUrl}" />
+          <meta property="twitter:url" content="${route.url}" />
+          <meta property="twitter:title" content="${route.title}" />
+          <meta property="twitter:description" content="${route.description}" />
+          <meta property="twitter:image" content="${route.imageUrl}" />
+        `;
+  else return "";
+};
+
+module.exports = { getMetaData };
